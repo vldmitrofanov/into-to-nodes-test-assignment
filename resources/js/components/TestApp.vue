e6e93c8c9645f84ac506c3bd0dbcfe6d1d824e52
Vladimir Mitrofanov <shirker2006@gmail.com>
2021-01-23 17:53:40 -0300

    Test App vue changes

--- resources/js/components/TestApp.vue
+++ resources/js/components/TestApp.vue
@@ -76,7 +76,7 @@ export default {
             this.expertsSrc = response.data.experts;
         },
         async fetchClients() {
-            this.clients = [];
+            //this.clients = [];
             const url = `/api/clients`;
             const response = await axios.get(url);
             this.clients = response.data.clients;
